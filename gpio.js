fe627661e7954ad72b05940d166de3c158a0c652
Kim <kim@richmondstudios.net>
2013-03-08 20:09:39 +0000

    merged

--- gpio.js
+++ gpio.js
@@ -1,9 +1,10 @@
-var gpio = require("pi-gpio");
+var gpio = require("gpio");
 
-var pin = 12;
-
-gpio.open(pin, "output", function(err) {
-    gpio.write(pin, 1, function() {
-        gpio.close(pin);
-    });
-});
+//var gpio11 = gpio.export(11, {direction:'out'});
+//var gpio12 = gpio.export(12, {direction:'out'});
+var gpio16 = gpio.export(16, {direction:'in',
+			      ready: function(){
+				  gpio16.on('change', function(val){
+				      console.log(val);
+				  });
+}});
